bcd30114ee8230410d8d275f90e0516af85521d7
Jonathan Crecencio <jonathancrecencio@outlook.com>
2020-10-15 23:05:47 -0300

    Incorporando os pontos no mapa com os dados temporarios

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -11,18 +11,32 @@ const icon = L.icon({
     popupAnchor: [170, 2]
 })
 
-// Criando popup
 
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage?id=1" class="choose-orphanage"><img src="/images/arrow-white.svg"> </a>')
+function addMarker({id, name, lat, lng}){
+    // Criando popup
+    const popup = L.popup({
+        closeButton: false,
+        className: 'map-popup',
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`${name}<a href="/orphanage?id=${id}"><img src="/images/arrow-white.svg"> </a>`)
 
+    // criando marcação
 
-// criando marcação
+    L.marker([lat, lng], { icon })
+        .addTo(map)
+        .bindPopup(popup)
+}
 
-L.marker([-23.9616031,-46.378863], { icon })
-    .addTo(map)
-    .bindPopup(popup)
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+
+orphanagesSpan.forEach((span) => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+
+    addMarker(orphanage)
+})
---
+++ src/database/fakedata.js
@@ -0,0 +1,55 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-23.9616031",
+        lng: "-46.378863",
+        name:"lar das meninas",
+        description:"Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1601299124026-30a104dc2858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+
+            "https://images.unsplash.com/photo-1565843728648-98a293377852?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+
+            "https://images.unsplash.com/photo-1565843248736-8c41e6db117b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+
+            "https://images.unsplash.com/photo-1566004100631-35d015d6a491?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+
+            "https://images.unsplash.com/photo-1591961169742-15818953881d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+
+            "https://images.unsplash.com/photo-1585576845235-ae4e33ec9f23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"
+        ],
+
+        instructions:"Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas das 8h até as 18h",
+        open_on_weekends: "1"
+
+    },
+
+    {
+        id: 2,
+        lat: "-23.9627529",
+        lng: "-46.3798306",
+        name:"lar dos meninos",
+        description:"Presta assistência a crianças de 02 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1601299124026-30a104dc2858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+
+            "https://images.unsplash.com/photo-1579099715183-a057711f3aa6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+
+            "https://images.unsplash.com/photo-1584922877726-155f07e7f8da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+
+            "https://images.unsplash.com/photo-1515435103043-eda5b0770648?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+
+            "https://images.unsplash.com/photo-1599876636829-f6780c077675?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+
+            "https://images.unsplash.com/photo-1579099715178-d89708832bce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"
+        ],
+
+        instructions:"Venha trazer carinho e amor para os meninos.",
+        opening_hours: "Horário de visitas das 9h até as 21h",
+        open_on_weekends: "0"
+
+    },
+    
+
+]
